bae5805428ce531bdcc8874aaf161925b75b9ce5
komagenn <kg19970831@gmail.com>
2021-09-15 14:51:10 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,6 +23,6 @@ class Item < ApplicationRecord
     validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :shipping_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"}
     validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
   end
