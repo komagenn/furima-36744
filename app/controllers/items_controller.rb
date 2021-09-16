4cc3600316202e87caf36121a7d4a44d1c410d05
komagenn <89065599+komagenn@users.noreply.github.com>
2021-09-16 14:57:14 +0900

    商品情報編集機能1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new ,:edit]
+  before_action :set_item, only: [:show ,:edit,:update]
   def index
     @items = Item.order(id: "DESC")
 
@@ -18,19 +19,16 @@ class ItemsController < ApplicationController
     end
   end
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
      end
      
   end
   def update
-     @item = Item.find(params[:id])
-    if @item.update(item_params)
+     if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render :edit
@@ -42,6 +40,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :explain, :price, :image, :genre_id, :status_id, :shipping_id, :prefecture_id, :delivery_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
