a5694e26353467ccd402eea8b7c6b64489cfc2c6
komagenn <89065599+komagenn@users.noreply.github.com>
2021-09-19 21:15:16 +0900

    商品購入機能３回目

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :purchase_id, :token, :item_id ,:user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :token, :item_id ,:user_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid.Include hyphen(-)"}
