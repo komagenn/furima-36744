e316c3f998dc1adc0ab48249d0a6cdf3d24db7a5
komagenn <89065599+komagenn@users.noreply.github.com>
2021-09-20 11:26:48 +0900

    修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,4 @@
-purclass PurchaseAddress
+class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :token, :item_id ,:user_id
 
